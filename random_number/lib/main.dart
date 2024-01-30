6c1a727707332a3c03e31433c4e6c7d3ad944795
Casey Meyvn <shatternight@protonmail.com>
2024-01-31 00:28:58 +0100

    Change to stateful widget

--- random_number/lib/main.dart
+++ random_number/lib/main.dart
@@ -5,15 +5,32 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var randInt = Random().nextInt(6) + 1;
+  MainAppState createState() => MainAppState();
+}
 
+class MainAppState extends State<MainApp> {
+  late int randInt;
+
+  @override
+  void initState() {
+    super.initState();
+    randInt = Random().nextInt(6) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+        home: GestureDetector(
+      onTap: () {
+        setState(() {
+          randInt = Random().nextInt(6) + 1;
+        });
+      },
+      child: Scaffold(
         body: Center(
           child: Text(
             randInt.toString(),
@@ -21,6 +38,6 @@ class MainApp extends StatelessWidget {
           ),
         ),
       ),
-    );
+    ));
   }
 }
